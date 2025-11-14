b113a395f1e55832cd611eb3cf0faefbdc0e981b
Minh Anh Nguyen <minhanhn@usf.edu>
2025-11-14 14:50:30 -0500

    completed task 1, join tables and sum total spend

--- challenge.sql
+++ challenge.sql
@@ -1,6 +1,11 @@
--- TASK 1
-SELECT first_name AS "First"
-from employees;
+-- TASK 1 - Top 5 Customers by Total Spend
+SELECT c.id, order_id, c.first_name, c.last_name, SUM(oi.quantity * oi.unit_price) AS total_spend 
+from customers AS c
+JOIN orders AS o ON c.id = o.customer_id
+JOIN order_items AS oi ON o.id=oi.order_id
+GROUP BY c.id
+ORDER BY sum(unit_price) desc
+LIMIT 5;
 
 --TASK 2
 SELECT * From customers;
