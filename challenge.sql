5ab79dbdcc7d32a1e6def284a6f8e8e4e9960511
Minh Anh Nguyen <minhanhn@usf.edu>
2025-11-14 15:46:16 -0500

    COMPLETED task 3 and 4, using count and group by and orderby

--- challenge.sql
+++ challenge.sql
@@ -13,3 +13,21 @@ FROM products AS p
 JOIN order_items AS oi ON p.id = oi.product_id
 GROUP BY p.category
 ORDER BY total_revenue DESC;
+
+-- TASK 3 - Employees Earning Above Their Department Average
+SELECT e.id, e.first_name, e.last_name, e.salary, d.name
+FROM employees AS e
+JOIN departments AS d ON e.department_id = d.id
+WHERE e.salary > (SELECT AVG(salary) 
+                FROM employees
+                WHERE department_id = e.department_id)
+ORDER BY d.name, e.salary DESC;
+
+-- TASK 4 - Cities with the Most Loyal Customers
+SELECT city, COUNT(DISTINCT c.id) AS loyal_customers
+FROM customers AS c
+JOIN orders AS o ON c.id = o.customer_id
+WHERE c.loyalty_level = 'Gold'
+GROUP BY c.city
+ORDER BY loyal_customers DESC
+;
